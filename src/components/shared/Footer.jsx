d46f1ecdbdfccc7b16b8e3302033abdfe85ac3f8
Alan Thomas <acthomas2000@gmail.com>
2024-08-13 13:29:07 -0700

    Fixed footer alignment and responsiveness

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 function Footer() {
     return (
         <footer className="max-w-[1600px] mx-auto w-full bg-react-blue flex flex-col px-10 pt-10 h-[40vh] justify-between">
-            <div className="flex justify-between">
-                <div>
+            <div className="flex flex-col md:flex-row justify-between">
+                <div className="flex flex-col self-center md:self-start">
                     <h2 className="text-3xl lg:text-5xl font-semibold pb-10">Let's Connect !</h2>
-                    <div className="flex gap-2 md:transition-transform md:duration-300 md:flex md:flex-row">
+                    <div className="flex gap-2 md:transition-transform md:duration-300 justify-between md:justify-start">
                         <a href="https://github.com/alantom10" target="_blank" rel="noopener noreferrer" className="border rounded-full w-10 h-10 flex justify-center items-center transition-colors duration-700 transform text-white shadow-md shadow-slate-950 hover:border-white hover:text-white">
                             <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
@@ -29,7 +29,7 @@ function Footer() {
                         </a>
                     </div>
                 </div>
-                <div className="flex flex-col w-[450px] self-center">
+                <div className="flex flex-col w-4/5 md:w-[450px] self-center pt-20 md:pt-0">
                     <h3 className="text-l lg:text-2xl self-center">Subscribe to my newsletter</h3>
                     <div className="relative mt-4">
                         <input type="email" id="newsletter-email" name="newsletter-email" placeholder="example@example.com" className="w-full h-12 rounded-full p-4 text-black shadow-custom "/>
@@ -37,7 +37,7 @@ function Footer() {
                     </div>
                 </div>
             </div>
-            <p className="text-white text-sm pb-10 pt-6 self-center">© 2024 All Rights Reserved Alan Thomas</p>
+            <p className="text-white text-sm pb-10 self-center pt-20 md:pt-6">© 2024 All Rights Reserved Alan Thomas</p>
         </footer>  
     );
 };
